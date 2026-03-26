f543245d4c816913dd0393f6e627cdbd0fc02cd8
PlamenZashev <plamenzashev0@gmail.com>
2026-03-26 18:10:57 +0200

    Added function to shower

--- Scripts/Interactables/shower.gd
+++ Scripts/Interactables/shower.gd
@@ -1,4 +1,15 @@
 extends Area2D
 
+const HEALTH_INCREASE_PER_SECOND = 1.0
+var showering : bool = false
+
+func _ready() -> void:
+	$CanvasLayer/modeUI.hide
+	
+func _process(delta: float) -> void:
+	Global.get_bars().heal(delta * HEALTH_INCREASE_PER_SEC)
+
+
 func interact():
+	Global.get_bars().
 	
